1ca8220aee5c54dbd3bec8842b9781638f46424a
Augustin Mauroy <97875033+AugustinMauroy@users.noreply.github.com>
2023-01-14 18:56:55 +0100

    feat(next): init work

---
+++ pages/404.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <div className="wip">
+      <h1>Error | 404</h1>
+      <Link href="/">Go to home page</Link>
+    </div>
+  )
+}
---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="wip">
+      <h1>Work in progresse</h1>
+      <p>I'm Augustin. I'm currently working on this website</p>
+      <hr></hr>
+    </div>
+  )
+}
