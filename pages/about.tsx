ecb351e447071fde32fd7bf0a72ed395866afe0a
Augustin Mauroy <97875033+AugustinMauroy@users.noreply.github.com>
2023-01-14 23:59:51 +0100

    feat:(about): add page

---
+++ pages/about.tsx
@@ -0,0 +1,13 @@
+import mainStyle from '../styles/layout.module.css';
+
+/**
+ * 
+ * @todo: write the content about me
+ */
+export default function About() {
+  return (
+    <main className={mainStyle.main}>
+      <h1>About Me</h1>
+    </main>
+  )
+}
