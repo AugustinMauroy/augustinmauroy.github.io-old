fba72de3ccf085342fcf0792c1eaf1588b77bfb2
Augustin Mauroy <97875033+AugustinMauroy@users.noreply.github.com>
2023-01-14 23:59:12 +0100

    feat(index): starting content

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,35 @@
+import mainStyle from '../styles/layout.module.css';
+import style from '../styles/index.module.css';
+import Link from 'next/link';
+
+/**
+ * 
+ * @todo: improve the small about me
+ * @todo: add a github section
+ * @todo: add a twitter section
+ * @todo: add a picture of me
+ */
 export default function Home() {
   return (
-    <div className="wip">
-      <h1>Work in progresse</h1>
-      <p>I'm Augustin. I'm currently working on this website</p>
+    <main className={mainStyle.main}>
+      <div className={style.presentation}>
+        <img src='placeholder-img.png'></img>
+        <div>
+        <h1>Augustin Mauroy</h1>
+        <p>Hi 👋 I'm Augustin. I'm 16 year old.</p>
+        <Link href='/about/'>Read more about me</Link>
+        </div>
+      </div>
       <hr></hr>
-    </div>
+      <div>
+        <div>
+          <h2>Github</h2>
+          <p>Text about my github</p>
+        </div>
+        <div>
+          <h2>Twitter</h2>
+        </div>
+      </div>
+    </main>
   )
 }
