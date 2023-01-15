849fac56a06865c2f26b4336b9df7d8532151c5c
Augustin Mauroy <97875033+AugustinMauroy@users.noreply.github.com>
2023-01-15 21:45:28 +0100

    feat(header): add strong for header

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -5,7 +5,7 @@ import style from './index.module.css';
 export default function header(){
     return(
     <nav className={mainStyle.header}>
-            <Link href="/" className={style.link}>Home</Link>
+            <strong><Link href="/" className={style.link}>Home</Link></strong>
             <Link href="/about" className={style.link}>About</Link>
             <Link href="/projects" className={style.link}>Projects</Link>
     </nav>
