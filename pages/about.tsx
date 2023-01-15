187684e73774e4768589e8b9efd9ee2dcffe3fc5
Augustin Mauroy <97875033+AugustinMauroy@users.noreply.github.com>
2023-01-15 21:44:57 +0100

    Update about.tsx

--- pages/about.tsx
+++ pages/about.tsx
@@ -5,29 +5,8 @@
 export default function About() {
   return (
     <>
-      <div>
         <h1>About Me</h1>
         <p>I'm Augustin Mauroy...</p>
-      </div>
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis ullam necessitatibus praesentium accusamus, natus dicta nostrum voluptates, perspiciatis laboriosam officiis deleniti fugiat assumenda odit delectus, earum perferendis iste dolorem inventore?</p>
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis ullam necessitatibus praesentium accusamus, natus dicta nostrum voluptates, perspiciatis laboriosam officiis deleniti fugiat assumenda odit delectus, earum perferendis iste dolorem inventore?</p>
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis ullam necessitatibus praesentium accusamus, natus dicta nostrum voluptates, perspiciatis laboriosam officiis deleniti fugiat assumenda odit delectus, earum perferendis iste dolorem inventore?</p>
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis ullam necessitatibus praesentium accusamus, natus dicta nostrum voluptates, perspiciatis laboriosam officiis deleniti fugiat assumenda odit delectus, earum perferendis iste dolorem inventore?</p>
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis ullam necessitatibus praesentium accusamus, natus dicta nostrum voluptates, perspiciatis laboriosam officiis deleniti fugiat assumenda odit delectus, earum perferendis iste dolorem inventore?</p>
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis ullam necessitatibus praesentium accusamus, natus dicta nostrum voluptates, perspiciatis laboriosam officiis deleniti fugiat assumenda odit delectus, earum perferendis iste dolorem inventore?</p>
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis ullam necessitatibus praesentium accusamus, natus dicta nostrum voluptates, perspiciatis laboriosam officiis deleniti fugiat assumenda odit delectus, earum perferendis iste dolorem inventore?</p>
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis ullam necessitatibus praesentium accusamus, natus dicta nostrum voluptates, perspiciatis laboriosam officiis deleniti fugiat assumenda odit delectus, earum perferendis iste dolorem inventore?</p>
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis ullam necessitatibus praesentium accusamus, natus dicta nostrum voluptates, perspiciatis laboriosam officiis deleniti fugiat assumenda odit delectus, earum perferendis iste dolorem inventore?</p>
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis ullam necessitatibus praesentium accusamus, natus dicta nostrum voluptates, perspiciatis laboriosam officiis deleniti fugiat assumenda odit delectus, earum perferendis iste dolorem inventore?</p>
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis ullam necessitatibus praesentium accusamus, natus dicta nostrum voluptates, perspiciatis laboriosam officiis deleniti fugiat assumenda odit delectus, earum perferendis iste dolorem inventore?</p>
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis ullam necessitatibus praesentium accusamus, natus dicta nostrum voluptates, perspiciatis laboriosam officiis deleniti fugiat assumenda odit delectus, earum perferendis iste dolorem inventore?</p>
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis ullam necessitatibus praesentium accusamus, natus dicta nostrum voluptates, perspiciatis laboriosam officiis deleniti fugiat assumenda odit delectus, earum perferendis iste dolorem inventore?</p>
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis ullam necessitatibus praesentium accusamus, natus dicta nostrum voluptates, perspiciatis laboriosam officiis deleniti fugiat assumenda odit delectus, earum perferendis iste dolorem inventore?</p>
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis ullam necessitatibus praesentium accusamus, natus dicta nostrum voluptates, perspiciatis laboriosam officiis deleniti fugiat assumenda odit delectus, earum perferendis iste dolorem inventore?</p>
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis ullam necessitatibus praesentium accusamus, natus dicta nostrum voluptates, perspiciatis laboriosam officiis deleniti fugiat assumenda odit delectus, earum perferendis iste dolorem inventore?</p>
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis ullam necessitatibus praesentium accusamus, natus dicta nostrum voluptates, perspiciatis laboriosam officiis deleniti fugiat assumenda odit delectus, earum perferendis iste dolorem inventore?</p>
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis ullam necessitatibus praesentium accusamus, natus dicta nostrum voluptates, perspiciatis laboriosam officiis deleniti fugiat assumenda odit delectus, earum perferendis iste dolorem inventore?</p>
-
     </>
   )
 }
