d8bfa2ca3a4f6ec65adb1706fb3fed69ac51b3a1
Augustin Mauroy <97875033+AugustinMauroy@users.noreply.github.com>
2023-01-17 22:11:02 +0100

    feat(head): use next/Head

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,19 @@ import Header from '../components/header'
 import Footer from '../components/footer'
 import type { AppProps } from 'next/app'
 import mainStyle from '../styles/layout.module.css';
+import Head from 'next/head'
 import '../styles/globals.css'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
     return (<>
-        <head>
+        <Head>
+            <link rel="icon" href="/logo.ico" sizes="any" />
             <title>Augustin's Portfolio</title>
-        </head>
+            <link rel="icon" href="/logo.svg" type="image/svg+xml" />
+            <link rel="apple-touch-icon" href="/logo.svg" />
+            <meta name="description" content="Augustin's Portfolio" />
+            <meta name="og:title" content="Augustin's Portfolio" />
+        </Head>
         <div className={mainStyle.body}>
         <Header />
         <main className={mainStyle.main}>
