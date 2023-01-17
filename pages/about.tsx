df9a3fd408bed3425ab33eb1628b7462881bba01
Augustin Mauroy <97875033+AugustinMauroy@users.noreply.github.com>
2023-01-17 22:11:25 +0100

    chore(about):Introduce text

--- pages/about.tsx
+++ pages/about.tsx
@@ -6,7 +6,8 @@ export default function About() {
   return (
     <>
         <h1>About Me</h1>
-        <p>I'm Augustin Mauroy...</p>
+        <p>I'm Augustin Mauroy, I'm 16 year old. I'm student.</p>
+        <p>I'm a developer, I'm learning web development.</p>
     </>
   )
 }
