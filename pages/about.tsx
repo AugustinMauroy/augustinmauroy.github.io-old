03d41d7cde5c596b59443fb0bbdca8f2d65c918f
Augustin Mauroy <97875033+AugustinMauroy@users.noreply.github.com>
2023-01-18 19:56:58 +0100

    feat(mdx): adding support of mdx

--- pages/about.tsx
+++
@@ -1,9 +0,0 @@
-export default function About() {
-  return (
-    <>
-        <h1>About Me</h1>
-        <p>I'm Augustin Mauroy, I'm 16 year old. I'm student.</p>
-        <p>I'm a developer, I'm learning web development.</p>
-    </>
-  )
-}
