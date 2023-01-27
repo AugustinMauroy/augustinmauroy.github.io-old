65e928d362fdd2caa28b58e650fd4a87c1e7ea8d
Augustin Mauroy <97875033+AugustinMauroy@users.noreply.github.com>
2023-01-27 23:22:47 +0100

    feat(footer): update copy right system

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -1,9 +1,18 @@
 import style from '../../styles/layout.module.css';
 
 export default function footer(){
+    const date = (startingDate: number): String  => {
+        const date = new Date();
+        const year = date.getFullYear();
+        if(startingDate > year){
+            return `${year} - ${startingDate}`
+        }
+        return year.toString();
+    };
+
     return(
     <footer  className={style.footer}>
-        <p>Augustin Mauroy ©</p>
+        <p>Augustin Mauroy © {date(2023)}</p>
     </footer>
     )
 }
