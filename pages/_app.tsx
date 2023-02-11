704e53a046a1864ffaa25748fc362ce5184cbfeb
Augustin Mauroy <97875033+AugustinMauroy@users.noreply.github.com>
2023-02-11 23:34:04 +0100

    add something

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,7 +15,7 @@ export default function MyApp({ Component, pageProps }: AppProps) {
     return (
     <div className={font.className}>
         <Head>
-            <title>Augustin's Portfolio</title>
+            <title>Augustin&apos;s Portfolio</title>
             <link rel="icon" type="image/x-icon" href="logo.ico" />
             <link rel="icon" type="image/xml-svg" href="logo.svg" />
             <meta name="description" content="Augustin's Portfolio" />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,28 @@
 import Link from 'next/link';
+import Image from 'next/image';
 import style from '../styles/index.module.css';
+import { FaGithub, FaTwitter } from 'react-icons/fa';
 
 export default function Home() {
   return (
   <>
   <article className={style.main}>
     <div className={style.presentation}>
-        <img src='me.jpeg'></img>
+        <Image src='/me.jpeg' alt='an profile picture' width={200} height={200}/>
         <div>
         <h1>Augustin Mauroy</h1>
-        <p>Hi 👋 I'm Augustin. I'm 16 year old. I'm full stack web dev.</p>
+        <p>Hi 👋 I&apos;m Augustin. I&apos;m 16 year old. I&apos;m full stack web dev.</p>
         <Link href='/about/'>Read more about me</Link>
         </div>
       </div>
       <div className={style.social}>
         <div>
-          <h2>Github</h2>
+          <h2><FaGithub /> Github</h2>
           <p>You can found my Github account. You can find my projects and the projects I have contributed to.</p>
           <Link href='https://github.com/augustinmauroy' target='_blank'>Look up my github Account</Link>
         </div>
         <div>
-          <h2>Twitter</h2>
+          <h2><FaTwitter /> Twitter</h2>
           <p>In my Twitter account you can found some communication</p>
           <Link href='https://twitter.com/_August1_' target='_blank'>Look up my twitter Account</Link>
         </div>
