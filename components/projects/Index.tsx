bc52fb65b5eac567b340b8f6f6251236ed8a81ba
Augustin Mauroy <97875033+AugustinMauroy@users.noreply.github.com>
2023-02-13 23:24:06 +0100

    feat(project): add component

---
+++ components/projects/Index.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import Styles from "./index.module.css";
+
+type TypeProjects = {
+    children: {
+        title: string;
+        description: string;
+        whatIHaveDone: string;
+        link: string;
+        url: string;
+        img?: string;
+    };
+};
+
+export default function Projects({ children }: TypeProjects): JSX.Element{
+    return (
+        <div className={Styles.project}>
+            <div className={Styles.text}>
+            <h2>{children.title}</h2>
+            <p>{children.description}</p>
+            <p>{children.whatIHaveDone}</p>
+            <Link href={children.link} />
+            </div>
+            {children.img? <img src={children.img} alt={children.title} /> : null}
+        </div>
+    );
+};
+
--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,16 +1,29 @@
-export default function Projects(): JSX.Element {
+import Projects from '../components/projects/Index';
+
+export default function ProjectsPage(): JSX.Element {
     return (
         <>
             <h1>My project</h1>
             <p>This page contains projects I have worked on or those I have created.</p>
-            <h2>Nodejs.dev</h2>
-            <p>A redesign of Nodejs.org built using Gatsby.js with React.js, TypeScript, and Remark.</p>
-            <p>I have work on desing and i18n (french) and fixing all are wrong.</p>
-            <a href='https://github.com/nodejs/nodejs.dev'>nodejs/nodejs.dev</a>
-            <h2>Nodejs.org</h2>
-            <p>The Node.js website.</p>
-            <p>I have work in I18n and i fixing some think in the website.</p>
-            <a href='https://github.com/nodejs/nodejs.org'>nodejs/nodejs.org</a>
+            <Projects>
+                {{
+                    title: 'Nodejs.dev',
+                    description: 'A redesign of Nodejs.org built using Gatsby.js with React.js, TypeScript, and Remark.',
+                    whatIHaveDone: 'I have work on desing, adding feature and i18n (french) and fixing all are wrong .',
+                    link: 'nodejs/nodejs.dev',
+                    url: 'https://github.com/nodejs/nodejs.dev',
+                    img: '/nodejs-dev.png'
+                }}
+            </Projects>
+            <Projects>
+                {{
+                    title: 'Nodejs.org',
+                    description: 'The Node.js website.',
+                    whatIHaveDone: 'I have work in I18n and i fixing some think in the website.',
+                    link: 'nodejs/nodejs.org',
+                    url: 'htps://github.com/nodejs/nodejs.org'
+                }}
+            </Projects>
         </>
     );
 }
