dca6c488e2ff7cda74a467511b4df4bb01eae272
Augustin Mauroy <97875033+AugustinMauroy@users.noreply.github.com>
2023-03-27 00:11:59 +0200

    fix(build)

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,5 +1,5 @@
 import Hero from '@/components/Hero';
-import Projects from '@/components/Projects';
+import Projects from '../components/Projects';
 
 export default function ProjectsPage(): JSX.Element {
     return (
