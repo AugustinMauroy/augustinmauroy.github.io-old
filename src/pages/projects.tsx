2bf754b683a811349ce3e13c9aa510cbff3c91ab
Augustin Mauroy <augustin.mauroy@outlook.fr>
2023-04-30 11:14:18 +0200

    Update projects.tsx

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,4 +1,4 @@
-import Projects from '@/components/Projects';
+import Projects from '../components/Projects';
 import styles from '@/styles/pages/content.module.scss';
 
 export default function PageProjects(): JSX.Element {
